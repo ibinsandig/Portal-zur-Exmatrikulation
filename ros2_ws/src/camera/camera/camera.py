4623d4050571908bbf915abad3f77e141bf13af9
ibinsandig <lbraun0203@gmail.com>
2026-03-30 09:31:16 +0200

    camera node init

---
+++ ros2_ws/src/camera/camera/camera.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+import cv2 as cv
+
+class Camera(Node):
+    def init(self):
+        super().__init__('camera')
+
+        self.publisher = self.create_publisher(Image, '/obj_data', 10)
+        timer_time = 1/30   # sek
+
+        path_camera = 0     # /dev/video0 
+
+        try:
+            self.img = cv.VideoCapture(path_camera)
+        except Exception as e:
+            self.get_logger().error(f'Fehler beim Initialiesieren der Kamera: {str(e)}')
+            raise e
+        
+        self.img.set(cv.CAP_PROP_BUFFERSIZE, 1)
+        
+        self.data = self.create_timer(timer_time, self.timer_callback)
+
+        self.get_logger().info('Camera-Node gestartet')
+
+    def read_camera():
+        pass
+
+    def timer_callback(self):
+        pass
+
+def main()
+
+if __name__ == 'main':
+    main()
