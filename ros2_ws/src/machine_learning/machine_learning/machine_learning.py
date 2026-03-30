abe4a7cf247b1f81e8d6c5598193dce6f805fa48
ibinsandig <lbraun0203@gmail.com>
2026-03-30 09:39:02 +0200

    erste node erstellt

---
+++ ros2_ws/src/machine_learning/machine_learning/machine_learning.py
@@ -0,0 +1,15 @@
+import rclpy
+from rclpy.node import Node
+
+class Machine_learning(Node):
+    def __init__(self):
+        super().__init__('machine_learning')
+
+    def timer_callback():
+        pass
+
+def main():
+    pass
+
+if __name__ == 'main':
+    main()
---
+++ ros2_ws/src/portal/portal/portal.py
@@ -0,0 +1,15 @@
+import rclpy
+from rclpy.node import Node
+
+class Portal(Node):
+    def __init__(self):
+        super().__init__('Portal')
+
+    def timer_callback():
+        pass
+
+def main():
+    pass
+
+if __name__ == 'main':
+    main()
