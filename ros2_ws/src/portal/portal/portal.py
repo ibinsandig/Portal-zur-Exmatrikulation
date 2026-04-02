7e8eadd9ac95cf3206a204edbac6140c17b69087
ibinsandig <lbraun0203@gmail.com>
2026-04-02 15:26:42 +0200

    pub sub portal

--- ros2_ws/src/portal/portal/portal.py
+++ ros2_ws/src/portal/portal/portal.py
@@ -1,21 +1,31 @@
 import rclpy
 from rclpy.node import Node
 from chaos_interfaces.msg import GoalData
+from ro45_portalrobot_interfaces.msg import RobotCmd
+from ro45_portalrobot_interfaces.msg import RobotPos
 class Portal(Node):
     def __init__(self):
         super().__init__('Portal')
 
-        self.subscription = self.create_subscription(
+        self.sub1 = self.create_subscription(
             GoalData,
             '/goal_data',
-            self.listener_callback,
+            self.callback1,
             10)
-        self.subscription
+        self.sub2 = self.create_subscription(
+            RobotPos,
+            '/robot_position',
+            self.callback2,
+            10)
 
+        self.publisher = self.create_publisher(RobotCmd, 'robot_command', 10)
 
         self.get_logger().info("Portal Node gestartet...")
 
-    def listener_callback(self, msg):
+    def callback1(self, msg):
+        pass
+
+    def callback2(self, msg):
         pass
 
 def main():
