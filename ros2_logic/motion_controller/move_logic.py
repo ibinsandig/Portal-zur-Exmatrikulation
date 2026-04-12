060e2a26fe97a43e83505ed7a05fdd61936d3f98
Patrice115 <patrice.wenzig@stud.hs-kempten.de>
2026-04-12 19:03:12 +0200

    eier

--- ros2_logic/motion_controller/move_logic.py
+++ ros2_logic/motion_controller/move_logic.py
@@ -2,7 +2,7 @@
 '''
 Grober Funktionsablauf:
 - IST-Daten werden dauerhaft aktuallisiert
-- Soll-Daten kommen rein
+- Soll-Daten kommen rein (max.20Hz)
 - Werden mit Ist-Daten verglichen
 IF (IST == SOLL) {Rückmeldung -> Zielstatus: job_finished = true}
 ELSE 
