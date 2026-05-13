dab1d111e3d258b7059c0b8cd57269f0defda380
ibinsandig <lbraun0203@gmail.com>
2026-05-13 12:46:55 +0200

    einmal alles bitte ohne majo

--- ros2_logic/camera/preprocessing.py
+++ ros2_logic/camera/preprocessing.py
@@ -12,18 +12,18 @@ class ImagePreprocessor:
 
         self.H = None
         self.H_inv = None
+        self.H_warp = None
+
         self.pts2_proportional = None
         self.width = None
         self.height = None
         self.img_warped = None
 
     def calibrate(self, init_frame):
-        corners = self.detect_aruco(init_frame)
+        corners, ids, rejected = self.detector.detectMarkers(init_frame)
         dstPoints = np.concatenate(corners, axis=1)
         self.H, _ = cv.findHomography(srcPoints=config_vm.SRC_COORDS, dstPoints=dstPoints, method=0)
-
-        self.H, status = cv.findHomography(srcPoints=config_vm.SRC_COORDS, dstPoints=dstPoints, method=0)
-        self.H_inv = np.linalg.inv(self.H)
+        self.H_inv = np.linalg.inv(H)
 
         pts1 = np.float32([
             corners[1][0][0],  # oben-links
@@ -44,55 +44,30 @@ class ImagePreprocessor:
         ], dtype=np.float32)
 
         offset = offset_raw.reshape(-1, 1, 2)
-
         pts1_2 = world_frame + offset
+        pts1_2_pixel = cv.perspectiveTransform(pts1_2, H)
 
-        pts1_2_pixel = cv.perspectiveTransform(pts1_2, self.H)
-
-        self.calculate_proportion(pts1_2_pixel)
-
-        H_warp = cv.getPerspectiveTransform(pts1_2_pixel, self.pts2_proportional)
-        self.img_warped = cv.warpPerspective(init_frame, H_warp, (self.width, self.height))
-
-    def warp_image(self, frame):
-        H_warp = cv.getPerspectiveTransform(pts1_2_pixel, self.pts2_proportional)
-        self.img_warped = cv.warpPerspective(init_frame, H_warp, (self.width, self.height))
-
-
-    def calculate_proportion(self, pixel):
-        min_x = np.min(pixel[:, 0, 0])
-        max_x = np.max(pixel[:, 0, 0])
-        min_y = np.min(pixel[:, 0, 1])
-        max_y = np.max(pixel[:, 0, 1])
+        min_x = np.min(pts1_2_pixel[:, 0, 0])
+        max_x = np.max(pts1_2_pixel[:, 0, 0])
+        min_y = np.min(pts1_2_pixel[:, 0, 1])
+        max_y = np.max(pts1_2_pixel[:, 0, 1])
 
         self.width = int(max_x - min_x)
         self.height = int(max_y - min_y)
 
-        self.pts2_proportional = np.float32([
+        pts2_proportional = np.float32([
             [0, 0],
             [self.width, 0],
             [0, self.height],
             [self.width, self.height]
         ])
+        self.H_warp = cv.getPerspectiveTransform(pts1_2_pixel, pts2_proportional)
 
-
-    def detect_aruco(self, frame):
-        corners, ids, rejected = self.detector.detectMarkers(frame)
-        return corners
+    def warp_image(self, frame):
+        self.img_warped = cv.warpPerspective(frame, self.H_warp, (self.width, self.height))
 
     def pixel_to_world(self, pixel):
-        
-        world = cv.perspectiveTransform(self.H_inv, pixel)
-        
-        return world
-    
-    def world_to_pixel(self, world):
-
-        pixel_coords = cv.perspectiveTransform(self.H, world)
-
-        return pixel_coords
-
-
+        pass
 
     def segment_object(self, frame):
         pass
