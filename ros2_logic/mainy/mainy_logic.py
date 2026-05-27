2a58898253f927bb1c8f306195471cd1d53e84dd
Patrice115 <patrice.wenzig@stud.hs-kempten.de>
2026-05-27 17:20:51 +0200

    pass

---
+++ ros2_logic/mainy/mainy_logic.py
@@ -0,0 +1,19 @@
+
+
+
+#=============================================================================
+
+class MainyLogic():
+    def __init__(self):
+        pass
+
+
+
+
+    def init_wait(self, init):
+        if init == True: 
+            self.init_done = True
+
+    def auftragseingang(self, id_obj, v_obj, goal_coord_x, goal_coord_y, goal_coord_z):
+        
+
--- ros2_ws/src/chaos_nodes/chaos_nodes/mainy.py
+++ ros2_ws/src/chaos_nodes/chaos_nodes/mainy.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from chaos_interfaces.msg import ObjDataDeluxe
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Point32
+from ros2_logic.mainy import mainy_logic
 
 
 #================================================================================================================
@@ -15,7 +16,7 @@ class Mainy(Node):
         self.sub_obj_data_deluxe = self.create_subscription(
             ObjDataDeluxe,
             '/obj_data_deluxe',
-            self.listener_callback,
+            self.obj_data_delux,
             10)
         
         self.sub_goal_reached = self.create_subscription(
@@ -27,17 +28,29 @@ class Mainy(Node):
         self.sub_init_done = self.create_subscription(
             Bool,
             '/init_done',
+            self.init_done,
             10)
         
         #========================================================================================================
 
-        self.pub_goal_coodrinates = self.create_publisher(Point32, '/goal_coordinates', 10)
+        self.pub_goal_coordinates = self.create_publisher(Point32, '/goal_coordinates', 10)
         self.pub_goal_gripper = self.create_publisher(Bool, '/goal_gripper', 10)
 
         #========================================================================================================
         
-        self.goal_reached = None
+        self.goal_reached = False
+        self.init_done = False
 
+        self.v_obj = None
+        self.typ_obj = None
+        self.id_obj = None
+        self.t_obj = None
+        self.goal_coord_x = None
+        self.goal_coord_y = None
+        self.goal_coord_z = None
+
+
+        self.mainylogic = MainyLogic()
 
         #========================================================================================================
 
@@ -47,9 +60,27 @@ class Mainy(Node):
 
     def goal_reached(self, msg):
         self.goal_reached = msg.data
-      
 
 #================================================================================================================
+
+    def init_done(self, msg):
+        self.init_done = msg.data
+
+#================================================================================================================
+
+    def obj_data_delux(self, msg):
+        self.v_obj = msg.v_obj      #TODO: Hier muss der Richtige CUSTOM-MSG Typ hin!!
+        self.typ_obj = msg.obj_typ
+        self.id_obj = msg.id_obj
+        self.t_obj = msg.t_obj
+        self.goal_coord_x = msg.coord_x
+        self.goal_coord_y = msg.coord_y
+        self.goal_coord_z = msg.coord_z
+        
+        self.mainylogic.auftragseingang(self.id_obj, )
+#================================================================================================================
+
+
 
 def main():
     rclpy.init(args=None)
