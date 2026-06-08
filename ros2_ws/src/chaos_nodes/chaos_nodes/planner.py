1cc5cc8e8d1f05edbe0f42f92612cfac1742af85
ibinsandig <lbraun0203@gmail.com>
2026-06-09 01:22:21 +0200

    final für heute

--- ros2_ws/src/chaos_nodes/chaos_nodes/planner.py
+++ ros2_ws/src/chaos_nodes/chaos_nodes/planner.py
@@ -3,45 +3,34 @@ from rclpy.node import Node
 from chaos_topics.msg import ObjType
 from chaos_topics.msg import FuturePosition
 from chaos_topics.msg import ObjData
-from planner.preprocessing import PostProcessor
+from planner.postprocessing import PostProcessor
+from functools import partial
+
 
 class Planner(Node):
     def __init__(self):
         super().__init__('planner')
 
         self.sub_obj_type = self.create_subscription(
-            ObjType,
-            '/obj_type',
-            self.timer_callback,
-            10)
+            ObjType, '/objType', partial(self.listener_callback, topic_name='0'), 10)
+
         
         self.sub_future_position = self.create_subscription(
-            FuturePosition,
-            '/future_position',
-            self.timer_callback_callback,
-            10
-        ) 
+            FuturePosition, '/future_position', partial(self.listener_callback, topic_name='1'), 10)
 
         self.pub_obj_data = self.create_publisher(ObjData, '/obj_data', 10)
-        timer_time = 1/30   # sek
 
-        self.PrePro = PlanPreprocessor()
+        self.PrePro = PostProcessor()
 
-        self.data = self.create_timer(timer_time, self.timer_callback)
         self.get_logger().info('Planner-Node gestartet')
 
-    def timer_callback(self):
-        """Timer Callback - führt die Planungslogik aus"""
-        # Hier wird die Logik zum Planen basierend auf den empfangenen Daten implementiert
-        
-        # Beispiel: Generiere einen Dummy-Objektdaten-String
-        dummy_obj_data = ObjData()
-        dummy_obj_data.name = "dummy_object"
-        dummy_obj_data.x = 0.5
-        dummy_obj_data.y = 0.5
-        dummy_obj_data.z = 0.0
-        
-        self.pub_obj_data.publish(dummy_obj_data)
+    def listener_callback(self, topic_name):
+
+        #TODO Hier sollen Daten aus den beiden nodes ankommen. 1. Die erhaltenen DAten muss zueinander zu geordnet werden anhand der IDs. Sowie die Berechnung des Offfsets zum greifen des objekt (id, theta) 2. wenn die das erste passiert ist werden die DAten in eine wartschlange gestellt wo das älteste Objekt als erstes an oberster Stell esteht. Neue Objekte werden hinten angestellt. 3. mit jedem durchlauf wird geschaut ob mit dem ersten objekt in der warteschlange eine geschwindigkeit berechnet werden aknn. wenn dies der fall ist wird dies gemacht und die DAten davon gepublished 4. die DAten werden solange gepublished, solange bis vom topic obj_finished die jeweilige ID zurückgegeben wird, dann wird das Objekt von der Lsite gelöscht.
+
+
+        pub_data = None
+        self.pub_obj_data.publish()
 
 def main(args=None):
     rclpy.init(args=args)
